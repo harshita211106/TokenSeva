55a200769d51dfd4397de8e2bed6c4eb64788ce8
harshita211106 <114283416+harshita211106@users.noreply.github.com>
2026-05-26 11:21:47 +0530

    feature: added create queue api

--- backend/server.js
+++ backend/server.js
@@ -23,5 +23,5 @@ app.get("/",(req,res)=>{
 const port=process.env.PORT;
 
 app.listen(port,()=>{
-    console.log(`server is running on port: ${port}`);
+    console.log(`server is running on port: http://localhost:${port}`);
 })
