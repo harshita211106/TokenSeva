aab54ff3fb652f130d7ac8c50c1b9647307c3c44
harshita211106 <114283416+harshita211106@users.noreply.github.com>
2026-07-09 14:57:36 +0530

    created cards and code clean up

--- frontend/src/pages/AdminDashboard.jsx
+++ frontend/src/pages/AdminDashboard.jsx
@@ -7,7 +7,7 @@ import {
 
 import socket from "../socket/frontendSocket"
 
-import QRCode from "react-qr-code";
+import QueueCard from "../components/QueueCard";
 
 
 function AdminDashboard() {
@@ -116,56 +116,27 @@ function AdminDashboard() {
             
              onClick={handleCreateQueue}
              disabled={loading}>
-                {loading ? "creating..." : "Create Queue"}
+                {loading ? "Creating..." : "Create Queue"}
             </button>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-8">
 
-            
-            
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-            {queues.map( (queue) => {
-                const queueEmpty = queue.currentServingToken >= queue.currentToken;
-                return(
+            {queues.map((queue) => {
 
-                <div key={queue._id}
-                className="bg-white rounded-xl shadow-md p-6"
-                >
-                    <h3 className="text-2xl font-semibold mb-4">{queue.name}</h3>
-                    <p className="text-sm text-gray-600 mb-2 text-center">Scan to Join Queue</p>
-                   <div className="mt-6 flex justify-center"> 
-                    <QRCode value={`http://localhost:5173/queue/${queue._id}`}/>
-                    </div>
-                <div className="space-y-2 mb-4">
-                    <p className="text-gray-700">
-                        Current Token: {queue.currentToken}
-                    </p>
+            const queueEmpty =
+            queue.currentServingToken >= queue.currentToken;
 
-                    <p className="text-gray-700">
-                        Serving: {queue.currentServingToken}
-                    </p>
-                </div>
-
-                    <button className={`mt-2 px-5 py-2 rounded-lg text-white transition
-                        ${
-                            queueEmpty
-                            ? "bg-gray-400 cursor-not-allowed"
-                            : "bg-green-600 hover:bg-green-700"
-                        }`}
-                     onClick={()=>handleNext(queue._id)}
-                     disabled={queueEmpty}
-                    >
-                        {queueEmpty
-                            ? "Queue Empty"
-                            : "Next Token"}
-                    
-                    </button>
-
-                    
-                </div>
-                )
-            }
-            )}
-            </div>
-            </div>
+             return (
+                <QueueCard
+                key={queue._id}
+                queue={queue}
+                queueEmpty={queueEmpty}
+                handleNext={handleNext}
+                 />
+                );
+                 })}
+                
+          </div>
+        </div>
         </div>
     );
 }
--- frontend/src/pages/CustomerQueue.jsx
+++ frontend/src/pages/CustomerQueue.jsx
@@ -6,6 +6,8 @@
     import {joinQueue} from "../services/customerQueueService";
 
     import { useParams } from "react-router-dom";
+
+    import TokenStatusCard from "../components/TokenStatusCard";
     
 
 
@@ -91,43 +93,14 @@
 
                 <h1 className="text-4xl font-bold mb-8 text-center">
                     Customer Queue</h1>
-
-                <div className="bg-white rounded-2xl shadow-md p-6 mb-8">
-
-                     {joinedData && (
-                         
-                    <>
-
-                    <h2 className="text-3xl font-bold text-blue-600 mb-4 text-center">
-                        Your Token: {joinedData.token}
-                    </h2>
-
-                    <div className="space-y-3 text-gray-700">
-                        
-                    <p className="text-lg">
-                        Position: {joinedData.position}
-                    </p>
-
-                    <p className="text-lg">
-                        People Ahead: {livePeopleAhead}
-                    </p>
-
-                    <p className="text-lg font-semibold text-orange-500">
-                        Estimated Wait: {liveEstimatedWait} mins
-                    </p>
-                    {livePeopleAhead === 0 && (
-                    <p className="text-green-600 font-bold text-xl mt-4 text-center">
-                        It's Your Turn!
-                    </p>
-                    )}
-
-                    </div>
-                    
-
-                    </>
-
-                        )}
-                </div>
+                
+                {joinedData && (
+                    <TokenStatusCard
+                        joinedData={joinedData}
+                        livePeopleAhead={livePeopleAhead}
+                        liveEstimatedWait={liveEstimatedWait}
+                    />
+                )}
             
         <div className="bg-white rounded-2xl shadow-md p-6">
         
